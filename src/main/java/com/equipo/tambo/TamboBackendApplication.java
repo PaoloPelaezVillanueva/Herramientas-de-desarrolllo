2b0591b7fb5637defa36ffddd64e7fd8ddb5c35c
Paolo Pelaez <paolopelaez13gmail.com>
2026-08-26 01:11:37 -0500

    Inicializa proyecto Spring Boot

---
+++ src/main/java/com/equipo/tambo/TamboBackendApplication.java
@@ -0,0 +1,13 @@
+package com.equipo.tambo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamboBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamboBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/equipo/tambo/TamboBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipo.tambo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamboBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
