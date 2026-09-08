33cacd960f25c6151273c0278cb1ec0ea273c4a5
Marko Encalada <markoencalada1@gmail.com>
2026-09-08 11:04:03 -0500

    Crea DTO Request/Response de Clientes

---
+++ src/main/java/com/equipo/tambo/cliente/dto/ClienteResponse.java
@@ -0,0 +1,23 @@
+package com.equipo.tambo.cliente.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteResponse {
+
+    private Long id;
+
+    private String nombre;
+
+    private String apellido;
+
+    private String dni;
+
+    private String email;
+
+    private String telefono;
+}
