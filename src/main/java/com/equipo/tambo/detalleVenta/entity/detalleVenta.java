5011e77dcc2c73720da0d27fb0a2d97cd12bb71b
Benjamin Reyes <benjaminra2104@gmail.com>
2026-09-10 01:14:54 -0500

    Agrego detalleVenta entity

---
+++ src/main/java/com/equipo/tambo/detalleVenta/entity/detalleVenta.java
@@ -0,0 +1,35 @@
+package com.abarrotes.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "detalle_venta")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DetalleVenta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idDetalleVenta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_venta", nullable = false)
+    private Venta venta;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto", nullable = false)
+    private Producto producto;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false)
+    private Double precioUnitario;
+
+    @Column(nullable = false)
+    private Double subtotal;
+}
