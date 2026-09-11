97554e575e2a9a8795e2b99b7fd760d9a4322b20
Benjamin Reyes <benjaminra2104@gmail.com>
2026-09-10 22:52:35 -0500

    IMPLEMENTACION REPOSITORY

---
+++ src/main/java/com/equipo/tambo/detalleVenta/repository/detalleVentaRepository.java
@@ -0,0 +1,13 @@
+package com.abarrotes.repository;
+
+import com.abarrotes.entity.DetalleVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DetalleVentaRepository
+        extends JpaRepository<DetalleVenta, Long> {
+
+    // Métodos personalizados (opcional)
+
+}
